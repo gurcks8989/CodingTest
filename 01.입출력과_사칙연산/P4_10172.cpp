434190884b2f764f49e398ff4f6fb79fbe73b44b
gurcks8989 <gurcks8989@naver.com>
2020-07-31 14:54:31 +0900

    ~Problem9

---
+++ 01.입출력과_사칙연산/P4_10172.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std ;
+
+void dog() ;
+
+int main(){
+
+    dog() ;    
+
+    return 0 ;
+}
+
+void dog(){
+
+    cout << "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|"<< endl ;
+
+    return ;
+}
