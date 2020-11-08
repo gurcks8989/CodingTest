8dba1b1ba14d092ac65c51028442359e5a6ce57a
권혁찬 <hyeokchani@hyeokchani-MacBook-Pro.local>
2020-11-08 13:41:59 +0900

    Fixed Problem.b

--- 대경권/P.cpp
+++ 대경권/P.cpp
@@ -1,8 +1,6 @@
 //P_16112
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -11,36 +9,37 @@ int n, k;
 
 // find_Sum_Experience_Point
 
+int compare(const void *a, const void *b) {
+	return *(int*)a - *(int *)b; 
+}
+
+
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio();
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n >> k ;
-    vector<int> v;
+    int num[n] ;
 
-    for(int i = 0 ; i < n ; i ++){
-		int tmp;
-		scanf("%d", &tmp);
-		v.push_back(tmp);
-    }
+    for(int i = 0 ; i < n ; i ++)
+        cin >> num[i] ;
 
     // 0번째는 0
     // 1번쨰는 제일 작은 수를 제외한 수의 합
     // 2번째는 제일 작은 수와 두번쨰 수를 제외한 수의 합
     // ... 이러한 것들이 k개 존재
 
-	sort(v.begin(), v.end());
+	qsort(num,n,sizeof(num[0]),compare);
 
-    long long int sum = 0 ;
-    int i = 0 ;
-    // v.at(j) * i   /(3*10^5*10*8/)
-    for(int j = 0 ; j < n ; j ++){
-        sum += (long long int)v.at(j) * (long long int)i ;
-        if (i < k)
-            i++;
+    int s = 0 ;
+
+    for(int i = 0 ; i < k ; i ++){
+        for(int j = i+1 ; j < n ; j ++){
+            s += num[j] ;
+        }
     }
     
-    printf("%lld\n", sum) ;
+    cout << s << "\n";
 
     return 0;
 }
