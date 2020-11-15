9d59f42ef43e27d1d21c27997ff680b7e9635997
gurcks8989 <21700057@handong.edu>
2020-11-15 20:49:10 +0900

    .

--- HPS/11week/B.P_2992_Big&SmallSize.cpp
+++ HPS/11week/B.P_2992_Big&SmallSize.cpp
@@ -23,13 +23,9 @@ using namespace std;
 예제 출력 1 
 165
 
-
-
 */
 
 
-int n, k;
-//int num[300000] ;
 
 // find_Sum_Experience_Point
 
@@ -37,32 +33,19 @@ int main(int argc, const char * argv[]) {
     ios::sync_with_stdio();
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n >> k ;
-    vector<int> v;
+    int X;
+    cin >> X ;
+    vector <int> v;
+    int Y = X ;
 
-    for(int i = 0 ; i < n ; i ++){
-		int tmp;
-		scanf("%d", &tmp);
-		v.push_back(tmp);
+    while(Y >= 10){
+        v.push_back(Y % 10) ;
+        Y /= 10 ;
     }
+    v.push_back(Y) ;
 
-    // 0번째는 0
-    // 1번쨰는 제일 작은 수를 제외한 수의 합
-    // 2번째는 제일 작은 수와 두번쨰 수를 제외한 수의 합
-    // ... 이러한 것들이 k개 존재
-
-	sort(v.begin(), v.end());
-
-    long long int sum = 0 ;
-    int i = 0 ;
-    // v.at(j) * i   /(3*10^5*10*8/)
-    for(int j = 0 ; j < n ; j ++){
-        sum += (long long int)v.at(j) * (long long int)i ;
-        if (i < k)
-            i++;
-    }
-    
-    printf("%lld\n", sum) ;
+    for(int i = 0 ; i < v.size(); i++)
+        printf("%d\n", v[i]);
 
     return 0;
 }
