3aea8a03f5cae3dc7dae71d01b210bb570618641
gurcks8989 <gurcks8989@naver.com>
2022-01-15 22:31:55 +0900

    push 204

---
+++ LeetCode/P204_Count_Primes.cpp
@@ -0,0 +1,52 @@
+/*
+204. Count Primes
+
+문제 출처:
+https://leetcode.com/problems/count-primes/
+
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+Example 2:
+
+Input: n = 0
+Output: 0
+
+Example 3:
+Input: n = 1
+Output: 0
+ 
+Constraints:
+0 <= n <= 5 * 106
+*/
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n == 0)
+            return 0 ;
+        
+        bool isPrime[n] ;
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        // Loop's ending condition is i * i < n instead of i < sqrt(n)
+        // to avoid repeatedly calling an expensive function sqrt().
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+};
