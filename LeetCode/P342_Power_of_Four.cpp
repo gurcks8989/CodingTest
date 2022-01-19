f4e0ecaaf653a903fd7606fb6dd3a55335f1252e
gurcks8989 <gurcks8989@naver.com>
2022-01-19 16:43:49 +0900

    258 342 860 12947

---
+++ LeetCode/P342_Power_of_Four.cpp
@@ -0,0 +1,33 @@
+/*
+342. Power of Four
+문제 출처: https://leetcode.com/problems/power-of-four/
+
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+An integer n is a power of four, if there exists an integer x such that n == 4x. 
+
+Example 1:
+Input: n = 16
+Output: true
+
+Example 2:
+Input: n = 5
+Output: false
+
+Example 3:
+Input: n = 1
+Output: true
+
+Constraints:
+-231 <= n <= 231 - 1
+*/
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1)
+            return true ; 
+        else if(n == 0 || (n & 3) != 0)
+            return false ;
+        else
+            return isPowerOfFour(n >> 2) ;
+    }
+};
