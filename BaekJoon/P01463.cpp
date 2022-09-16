428c99779764dc31553a74c15263b6e611de38f4
gurcks8989 <gurcks8989@naver.com>
2022-09-16 22:48:01 +0900

    09.16

--- BaekJoon/P01463.cpp
+++ BaekJoon/P01463.cpp
@@ -7,7 +7,8 @@
 X가 3으로 나누어 떨어지면, 3으로 나눈다.
 X가 2로 나누어 떨어지면, 2로 나눈다.
 1을 뺀다.
-정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 
+연산을 사용하는 횟수의 최솟값을 출력하시오.
 
 입력
 첫째 줄에 1보다 크거나 같고, 106보다 작거나 같은 정수 N이 주어진다.
@@ -26,6 +27,8 @@ X가 2로 나누어 떨어지면, 2로 나눈다.
 */
 
 #include <iostream>
+#include <algorithm>
+#define MAX 1000000
 
 using namespace std ;
 
@@ -33,17 +36,19 @@ int main(){
     ios::sync_with_stdio(false) ;
     cin.tie(NULL) ;
     cout.tie(NULL) ;
-    int X, cnt = 0 ;
-    cin >> X ;
-    while(X != 1){
-        if(X % 3 == 0)
-            X /= 3 ;
-        else if(X % 2 == 0)
-            X /= 2 ;
+    int dp[MAX+1] = {0, 0, 1, 1, } ;
+    for(int i = 4 ; i <= MAX ; i++){
+        if(i % 6 == 0)
+            dp[i] = min(dp[i/3], dp[i/2]) + 1 ;
+        else if(i % 3 == 0)
+            dp[i] = min(dp[i/3], dp[i-1]) + 1 ;
+        else if(i % 2 == 0)
+            dp[i] = min(dp[i/2], dp[i-1]) + 1 ;
         else
-            X -= 1 ;
-        cnt += 1 ;
+            dp[i] = dp[i-1] + 1 ;
     }
-    cout << cnt << "\n" ;
+    int X ;
+    cin >> X ;
+    cout << dp[X] << "\n" ;
     return 0 ;
 }
