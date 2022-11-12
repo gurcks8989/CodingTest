cde2190c44d822437cefb74ae6771ec6ccbd0a74
gurcks8989 <gurcks8989@naver.com>
2022-11-13 01:16:29 +0900

    11.13

--- BaekJoon/P01780.cpp
+++ BaekJoon/P01780.cpp
@@ -37,13 +37,54 @@ N×N크기의 행렬로 표현되는 종이가 있다.
 */
 
 #include <iostream>
+#define RANGE 2187
 
 using namespace std ;
+
+short paper[RANGE][RANGE] ;
+int N = 0 ;
+int counts[3] ;
+// 0 is -1, 1 is 0, 2 is 1 
+bool checkCut(int standard, pair<int, int> row, pair<int, int> col){
+    for(int i = row.first ; i <= row.second ; i++){
+        for(int j = col.first ; j <= col.second ; j++){
+            if(standard != paper[i][j])
+                return false ;
+        }
+    }
+    return true ;
+}
+
+void dividePaper(pair<int, int> row, pair<int, int> col){
+    int standard = paper[row.first][col.first] ;
+    if(checkCut(standard, row, col)){
+        standard != 1 ? (standard != 0 ? counts[0]++ : counts[1]++ ) : counts[2]++ ;
+        return ;
+    }
+    int n = (row.second - row.first) / 3 ;
+    pair<int, int> rowT = make_pair(row.first, row.first + n ) ;
+    pair<int, int> rowC = make_pair(row.first + n + 1, row.second - n - 1) ;
+    pair<int, int> rowB = make_pair(row.second - n, row.second) ;
+    pair<int, int> colL = make_pair(col.first, col.first + n) ;
+    pair<int, int> colC = make_pair(col.first + n + 1, col.second - n - 1) ;
+    pair<int, int> colR = make_pair(col.second - n, col.second) ;
+    dividePaper(rowT, colL) ;   dividePaper(rowT, colC) ;   dividePaper(rowT, colR) ;
+    dividePaper(rowC, colL) ;   dividePaper(rowC, colC) ;   dividePaper(rowC, colR) ;
+    dividePaper(rowB, colL) ;   dividePaper(rowB, colC) ;   dividePaper(rowB, colR) ;
+}
 
 int main(){
     ios::sync_with_stdio(false) ;
     cin.tie(NULL) ;
     cout.tie(NULL) ;
-
+    int N ;
+    cin >> N ;
+    for(int i = 0 ; i < N ; i++)
+        for(int j = 0 ; j < N ; j++)
+            cin >> paper[i][j] ;
+    pair<int, int> init(0, N-1);
+    dividePaper(init, init) ;
+    for(int i = 0 ; i < 3 ; i++)
+        cout << counts[i] << "\n" ;
     return 0 ;
 }
